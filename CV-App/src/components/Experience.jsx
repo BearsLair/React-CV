e23e92c6ce9eb133916a2796f59ffb0a8bfac34c
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-05-08 16:15:30 -0700

    Basic experience content in place

--- CV-App/src/components/Experience.jsx
+++ CV-App/src/components/Experience.jsx
@@ -1,5 +1,29 @@
 function Experience() {
-  return <></>;
+  return (
+    <div className="experience-div">
+      <h1 className="experience-title">Experience</h1>
+      <form>
+        <p className="item-label" htmlFor="company-name">
+          Company Name:
+        </p>
+        <input className="company-name-input" type="text" id="company-name" />
+        <p className="item-label" htmlFor="position-title">
+          Position Title:
+        </p>
+        <input className="pos-title-input" type="text" id="position-title" />
+        <p className="item-label">Main Responsiblities</p>
+        <textarea className="main-resp-input" id="main-resp" />
+        <p className="item-label" htmlFor="exp-start-date">
+          Start Date:
+        </p>
+        <input className="date-input" type="date" id="exp-start-date" />
+        <p className="item-label" htmlFor="exp-end-date">
+          End Date:
+        </p>
+        <input className="date-input" type="date" id="exp-end-date" />
+      </form>
+    </div>
+  );
 }
 
 export default Experience;
