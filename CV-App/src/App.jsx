f9c909fdbb87ac3541bc4d1d6177590cf83cfd9f
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-05-08 16:15:14 -0700

    imported App.css

--- CV-App/src/App.jsx
+++ CV-App/src/App.jsx
@@ -1,6 +1,7 @@
 import General from "./components/General.jsx";
 import Education from "./components/Education.jsx";
 import Experience from "./components/Experience.jsx";
+import "./styles/App.css";
 
 function App() {
   return (
