abfdc8a5bbb285453654d76d5d35697aac5fad91
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-05-12 22:22:48 -0700

    added useState hook and component tags to html

--- CV-App/src/App.jsx
+++ CV-App/src/App.jsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import General from "./components/General.jsx";
 import Education from "./components/Education.jsx";
 import Experience from "./components/Experience.jsx";
+import GeneralDisplay from "./components/GeneralDisplay.jsx";
 import "./styles/App.css";
 
 function App() {
+  const [generalInfo, setGeneralInfo] = useState({});
+
   return (
     <>
-      <General />
+      <General submitGeneral={setGeneralInfo} />
       <hr />
       <Education />
       <hr />
       <Experience />
+      <hr />
+      <hr />
+      <hr />
+      <GeneralDisplay />
     </>
   );
 }
