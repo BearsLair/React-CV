c03b107ebe233fc6428f809479de90b07ff62ecd
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-05-19 15:41:14 -0700

    commented out CV display

--- CV-App/src/App.jsx
+++ CV-App/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import General from "./components/General.jsx";
 import Education from "./components/Education.jsx";
 import Experience from "./components/Experience.jsx";
-import CV_Display from "./components/CV_Display.jsx";
+// import CV_Display from "./components/CV_Display.jsx";
 import "./styles/App.css";
 
 function App() {
